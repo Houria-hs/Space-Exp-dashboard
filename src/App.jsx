ae9569717870c8079db33ec9faa1833ad7e6df98
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-08-14 14:50:17 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css';
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center mt-50">
+      Hello, Tailwind!
+    </div>
+  )
+}
+
+export default App;
